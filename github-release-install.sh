448cd3fbbbee909c7bffc8f98a5e293aada806cd
Benjamin Sherman <benjamin@holyarmy.org>
2023-05-02 23:23:43 -0500

    feat: add wezterm with new github script

--- github-release-install.sh
+++ github-release-install.sh
@@ -6,42 +6,38 @@
 # example: https://github.com/wez/wezterm/releases
 #   ORG_PROJ would be "wez/wezterm"
 #
-# RPM_PRE is the first part of the rpm's filename
-# example: for file "wezterm-20230320_124340_559cb7b0-1.fedora37.x86_64.rpm"
-#   RPM_PRE would be "wezterm"
-#   NOTE: this may not always match the project name
-#
 # FILTER is used if there are more than one RPMs which match our arch/noarch.rpm pattern
 # example: wezterm builds a lot of rpms, but they are labeled by distro so we can filter
-#   FILTER would be "fedora37" to get the RPM build for fedora37
+#   FILTER would be "fedora36" to get the RPM build for fedora36 OR "fedora" should get
+#          the RPM build for fedora37 (or newest fedora build) due to a reverse sort
 
 ORG_PROJ=${1}
-RPM_PRE=${2}
+SUFFIX=${2}
 FILTER=${3}
 
+usage() {
+  echo "$0 ORG_PROJ SUFFIX [FILTER]"
+  echo "    ORG_PROJ - organization/projectname"
+  echo "    SUFFIX   - trailing part of rpm name: eg, x86_64rpm or noarch.rpm"
+  echo "    FILTER   - optional extra filter to further limit rpm selection"
+
+}
+
 if [ -z ${ORG_PROJ} ]; then
-  echo "$0 ORG_PROJ RPM_PRE are required"
+  usage
   exit 1
 fi
 
-if [ -z ${RPM_PRE} ]; then
-  echo "$0 ORG_PROJ RPM_PRE are required"
+if [ -z ${SUFFIX} ]; then
+  usage
   exit 2
 fi
 
-install() {
-  API=${1}
-  NAME=${2}
-  FILTER=${3}
-
-  #curl -sL ${API} | jq -r '.assets[].browser_download_url' | grep -E 'x64.rpm$|x86_64.rpm$|all.rpm$|noarch.rpm$' | grep "${FILTER}"
-  DLS=$(curl -sL ${API} | jq -r '.assets[].browser_download_url' | grep -E 'x64.rpm$|x86_64.rpm$|all.rpm$|noarch.rpm|Linux.rpm$' | grep "${FILTER}")
-  for DL in ${DLS}; do
-    # WARNING: in case of multiple matches, this only installs the first
-    echo "execute: rpm-ostree install \"${DL}\""
-    rpm-ostree install "${DL}"
-    break
-  done
-}
-
-install "https://api.github.com/repos/${ORG_PROJ}/releases/latest"  "${RPM_PRE}" "${FILTER}"
+API="https://api.github.com/repos/${ORG_PROJ}/releases/latest"
+RPM_URLS=$(curl -sL ${API} | jq -r '.assets[].browser_download_url' | grep -E "${SUFFIX}$" | grep "${FILTER}" |sort -r)
+for URL in ${RPM_URLS}; do
+  # WARNING: in case of multiple matches, this only installs the first (hopefully the newest, given the reverse sort)
+  echo "execute: rpm-ostree install \"${URL}\""
+  rpm-ostree install "${URL}"
+  break
+done
