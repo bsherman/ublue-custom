446c8b6a7b146f4d3dd954a46a950417cb1290d2
Benjamin Sherman <benjamin@holyarmy.org>
2023-09-08 09:24:15 -0500

    feat: remove frippery and notification banner extensions

--- usr/share/gnome-shell/extensions/notification-banner-reloaded@marcinjakubowski.github.com/utils.js
+++
@@ -1,9 +0,0 @@
-var PrefFields = {
-    ANCHOR_VERTICAL        : 'anchor-vertical',
-    ANCHOR_HORIZONTAL      : 'anchor-horizontal',
-    PADDING_VERTICAL       : 'padding-vertical',
-    PADDING_HORIZONTAL     : 'padding-horizontal',
-    ANIMATION_TIME         : 'animation-time',
-    ANIMATION_DIRECTION    : 'animation-direction',
-    ALWAYS_MINIMIZE        : 'always-minimized',
-};
