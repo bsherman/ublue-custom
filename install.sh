9fe636c6171c376ef71684e983a5fcd2df77b278
Benjamin Sherman <benjamin@holyarmy.org>
2024-01-15 23:49:26 -0600

    feat: add Prompt term (#21)
    only add Prompt for silverblue at the moment

--- install.sh
+++ install.sh
@@ -10,16 +10,31 @@ mkdir -p /var/lib/alternatives
 # Get required repos
 wget https://pkgs.tailscale.com/stable/fedora/tailscale.repo -O /etc/yum.repos.d/tailscale.repo
 if [ "sericea" == "${IMAGE_NAME}" ]; then
-    wget https://copr.fedorainfracloud.org/coprs/tofik/sway/repo/fedora-${RELEASE}/tofik-sway-fedora-${RELEASE}.repo -O /etc/yum.repos.d/copr_tofik-sway.repo
+  wget https://copr.fedorainfracloud.org/coprs/tofik/sway/repo/fedora-${RELEASE}/tofik-sway-fedora-${RELEASE}.repo -O /etc/yum.repos.d/copr_tofik-sway.repo
 fi
 
 # install kmods if F39 or newer
-if [[ "${FEDORA_MAJOR_VERSION}" -ge 39 ]]; then
+if [ "${FEDORA_MAJOR_VERSION}" -ge 39 ]; then
   for REPO in $(rpm -ql ublue-os-akmods-addons|grep ^"/etc"|grep repo$); do
     echo "akmods: enable default entry: ${REPO}"
     sed -i '0,/enabled=0/{s/enabled=0/enabled=1/}' ${REPO}
   done
   rpm-ostree install /tmp/akmods-rpms/*.rpm
+fi
+
+# Prompt Terminal
+if [ "silverblue" == "${IMAGE_NAME}" ]; then
+  if [ ${FEDORA_MAJOR_VERSION} -ge "39" ]; then
+    wget https://copr.fedorainfracloud.org/coprs/kylegospo/prompt/repo/fedora-$(rpm -E %fedora)/kylegospo-prompt-fedora-$(rpm -E %fedora).repo?arch=x86_64 -O /etc/yum.repos.d/_copr_kylegospo-prompt.repo && \
+    rpm-ostree override replace \
+    --experimental \
+    --from repo=copr:copr.fedorainfracloud.org:kylegospo:prompt \
+        vte291 \
+        vte-profile && \
+    rpm-ostree install \
+        prompt && \
+    rm -f /etc/yum.repos.d/_copr_kylegospo-prompt.repo
+  fi
 fi
 
 # run common packages script
