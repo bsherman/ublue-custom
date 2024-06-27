178ccad64bb6093f4fa0d28e704966cb47671eab
Benjamin Sherman <benjamin@holyarmy.org>
2024-06-27 16:04:18 -0500

    fix: no kernel suffix since no surface

--- initramfs.sh
+++ initramfs.sh
@@ -12,6 +12,6 @@ else
   rm -f /usr/lib/modprobe.d/nvidia*.conf
 fi
 
-QUALIFIED_KERNEL="$(rpm -qa | grep -P 'kernel-(|'"$KERNEL_SUFFIX"'-)(\d+\.\d+\.\d+)' | sed -E 's/kernel-(|'"$KERNEL_SUFFIX"'-)//')"
+QUALIFIED_KERNEL="$(rpm -qa | grep -P 'kernel-(\d+\.\d+\.\d+)' | sed -E 's/kernel-//')"
 /usr/libexec/rpm-ostree/wrapped/dracut --no-hostonly --kver "$QUALIFIED_KERNEL" --reproducible -v --add ostree -f "/lib/modules/$QUALIFIED_KERNEL/initramfs.img"
 chmod 0600 "/lib/modules/$QUALIFIED_KERNEL/initramfs.img"
