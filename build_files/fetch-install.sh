d9589fa74cd5de52a7e975e0120497bb3f75e05b
Benjamin Sherman <benjamin@holyarmy.org>
2024-09-14 20:16:59 -0500

    fix: re-add chezmoi

--- build_files/fetch-install.sh
+++ build_files/fetch-install.sh
@@ -2,6 +2,9 @@
 
 set -ouex pipefail
 
+# github direct installs
+/ctx/build_files/github-release-install.sh twpayne/chezmoi x86_64
+
 # Starship Shell Prompt
 curl -Lo /tmp/starship.tar.gz "https://github.com/starship/starship/releases/latest/download/starship-x86_64-unknown-linux-gnu.tar.gz"
 tar -xzf /tmp/starship.tar.gz -C /tmp
