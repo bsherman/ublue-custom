fce45c6921db23be1bb8bee9d787e5ed90e2b9e8
Benjamin Sherman <benjamin@holyarmy.org>
2024-09-28 14:56:06 -0500

    feat: use patched fwupd
    This is the normal upstream fwupd package with a single extra compile
    flag to continue to use fedora as the path by using the os-release ID
    (which is "fedora"), which allows customization of the name in
    os-release without breaking fwupd.

--- build_files/copr-repos.sh
+++ build_files/copr-repos.sh
@@ -9,6 +9,17 @@ curl -Lo /etc/yum.repos.d/ublue-os-staging-fedora-"${FEDORA_MAJOR_VERSION}".repo
 # Add repo
 curl -Lo /etc/yum.repos.d/_copr_sentry-switcheroo-control_discrete.repo https://copr.fedorainfracloud.org/coprs/sentry/switcheroo-control_discrete/repo/fedora-"${FEDORA_MAJOR_VERSION}"/sentry-switcheroo-control_discrete-fedora-"${FEDORA_MAJOR_VERSION}".repo
 
+# Fix for ID in fwupd
+if [[ "${FEDORA_MAJOR_VERSION}" -gt "39" ]]; then
+    rpm-ostree override replace \
+        --experimental \
+        --from repo=copr:copr.fedorainfracloud.org:ublue-os:staging \
+            fwupd \
+            fwupd-plugin-flashrom \
+            fwupd-plugin-modem-manager \
+            fwupd-plugin-uefi-capsule-data
+fi
+
 # Switcheroo patch
 rpm-ostree override replace \
     --experimental \
