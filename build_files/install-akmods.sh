295645df496e18cd95bd25e7f014c50d7589ff7e
Benjamin Sherman <benjamin@holyarmy.org>
2024-10-06 00:53:17 -0500

    chore: negativo17 image base, disabling v4l2loopback
    removed rpmfusion, so disabling v4l2loopback until i get it installing
    again

--- build_files/install-akmods.sh
+++ build_files/install-akmods.sh
@@ -4,13 +4,7 @@ set -ouex pipefail
 
 curl https://pkgs.tailscale.com/stable/fedora/tailscale.repo -o /etc/yum.repos.d/tailscale.repo
 
-curl -Lo /etc/yum.repos.d/negativo17-fedora-multimedia.repo https://negativo17.org/repos/fedora-multimedia.repo
 sed -i 's@enabled=0@enabled=1@g' /etc/yum.repos.d/_copr_ublue-os-akmods.repo
-
-# if [[ -n "${NVIDIA_TYPE:-}" ]]; then
-#     curl -L -o /etc/yum.repos.d/fedora-coreos-pool.repo \
-#         https://raw.githubusercontent.com/coreos/fedora-coreos-config/testing-devel/fedora-coreos-pool.repo
-# fi
 
 # Nvidia for gts/stable - nvidia
 if [[ "${NVIDIA_TYPE}" == "nvidia" ]]; then
@@ -24,10 +18,8 @@ fi
 rpm-ostree install \
     /tmp/akmods/kmods/*xone*.rpm \
     /tmp/akmods/kmods/*xpadneo*.rpm \
-    /tmp/akmods/kmods/*openrazer*.rpm \
-    /tmp/akmods/kmods/*v4l2loopback*.rpm
-
-sed -i 's@enabled=1@enabled=0@g' /etc/yum.repos.d/negativo17-fedora-multimedia.repo
+    /tmp/akmods/kmods/*openrazer*.rpm
+#    /tmp/akmods/kmods/*v4l2loopback*.rpm
 
 # ZFS for gts/stable
 if [[ ${AKMODS_FLAVOR} =~ "coreos" ]]; then
